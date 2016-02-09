739a814fc793587f5ed5d94eca10679bf9d26118
Mo Baig <mo@mobaig.com>
2016-02-09 15:36:35 +0000

    Standardise naming across Education view templates and add routes
    Drop version tags and setup individual routes for each view.

--- app/routes.js
+++ app/routes.js
@@ -1,14 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+// Example:
+// router.get('/examples/template-data', function (req, res) {
+//   res.render('examples/template-data', { 'name' : 'Foo' });
+// });
+
 router.get('/', function (req, res) {
   res.render('index');
 });
 
-router.get('/examples/template-data', function (req, res) {
-  res.render('examples/template-data', { 'name' : 'Foo' });
+router.get('/education', function (req, res) {
+  res.render('education');
 });
 
+router.get('/childcare-and-early-years', function (req, res) {
+  if ( req.query.section === 'detailed' ) {
+    res.render('childcare-and-early-years_detailed');
+  }
+  else if ( req.query.section === 'policy' ) {
+    res.render('childcare-and-early-years_policy');
+  }
+  else if ( req.query.section === 'publications' ) {
+    res.render('childcare-and-early-years_publications');
+  }
+  else {
+    res.render('childcare-and-early-years');
+  }
+});
+
+router.get('/early-years-settings', function (req, res) {
+  res.render('early-years-settings');
+});
+
+router.get('/childminders', function (req, res) {
+  if ( req.query.section === 'detailed' ) {
+    res.render('childminders_detailed');
+  }
+  else if ( req.query.section === 'policy' ) {
+    res.render('childminders_policy');
+  }
+  else if ( req.query.section === 'publications' ) {
+    res.render('childminders_publications');
+  }
+  else {
+    res.render('childminders');
+  }
+});
 // add your routes here
 
 module.exports = router;
